e1992a01f1c1c05c357e3feaf236712e18c61e1e
Roberto Nicoletti <roberto.urias@gmail.com>
2020-08-25 21:33:34 -0300

    criando primeira tabela inserindo dados no mongodb

--- producthunt/server.js
+++ producthunt/server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();
 
 // Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/producthuntapi', { useNewUrlParser: true });
+requireDir('./src/models');
 
+const Product = mongoose.model('Product');
 // Primeira rota
 app.get('/', (req, res) => {
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with',
+        url: 'http://github.com/facebook/react-native'
+    });
     res.send('Hello node');
 });
 
