4c2f8de9853ae2de39dc171731194e7168e34110
Roberto Nicoletti <roberto.urias@gmail.com>
2020-08-25 21:16:51 -0300

    connect db

--- producthunt/server.js
+++ producthunt/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando o App
 const app = express();
 
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/producthuntapi', { useNewUrlParser: true });
+
+// Primeira rota
 app.get('/', (req, res) => {
     res.send('Hello node');
 });
